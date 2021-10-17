32a8776d454e63a77b24d5e08c0cff3cb29c8c34
luyuqiuchen <luyuqiuchen@163.com>
2021-10-17 15:42:27 +0800

    初始化

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/luyuqiuchen/helloci
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,14 @@
+/**
+ * @Author: YQC
+ * @Description:
+ * @File:  main
+ * @Version: 1.0.0
+ * @Date: 2021/10/16 17:32
+ */
+package helloci
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Github Actions!")
+}
